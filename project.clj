244ec506d3602153a4950756ab6c6ab0ff585c93
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2016-07-17 21:04:38 +0200

    pump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject minhtuannguyen/cljocker "0.1.7"
+(defproject minhtuannguyen/cljocker "0.1.8-SNAPSHOT"
   :description "a dockerfile dsl written in clojure"
   :url ""
   :license {:name "Eclipse Public License"
